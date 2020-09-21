5d5b492e9c563ac38e835d5d6bb6b4eb079f7afe
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-09-22 00:11:17 +0200

    Make Add, sub, remove actions and change view when action made

--- src/components/CartComponent.js
+++ src/components/CartComponent.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeItem, addQuantity, subtractQuantity } from '../redux/cartActions'
 
-const mapStateToProps = (state) => {
-    return {
-        products: state.addedProducts,
-        total: state.total
-    }
-}
 class CartComponent extends Component {
+
+    //to remove the product completely from cart
+    handleRemove = (id) => {
+        this.props.removeItem(id);
+    }
+    //to add the quantity of product
+    handleAddQuantity = (id) => {
+        this.props.addQuantity(id);
+    }
+    //to substruct from the quantity of product
+    handleSubtractQuantity = (id) => {
+        this.props.subtractQuantity(id);
+    }
 
     render() {
         const cartProducts = this.props.products.map((cartProduct) => {
@@ -19,23 +27,23 @@ class CartComponent extends Component {
                             <h3>{cartProduct.title}</h3>
                             <p className="itemNumber">{cartProduct.code}</p>
                             <p className="itemQuantity">
-                                <i className="fa fa-minus" aria-hidden="true" > &nbsp;</i>
+                                <i className="fa fa-minus" aria-hidden="true" onClick={() => { this.handleSubtractQuantity(cartProduct.id) }}> &nbsp;</i>
                                 <input type="text" className="qty" value={cartProduct.quantity} disabled />
-                                <i className="fa fa-plus" aria-hidden="true" >&nbsp;</i>
+                                <i className="fa fa-plus" aria-hidden="true" onClick={() => { this.handleAddQuantity(cartProduct.id) }}>&nbsp;</i>
                             </p>
                         </div>
                         <div className="prodTotal cartSection">
                             <p>${cartProduct.price}</p>
                         </div>
                         <div className="cartSection removeWrap">
-                            <i className="fa fa-times" aria-hidden="true" ></i>
+                            <i className="fa fa-times" aria-hidden="true" onClick={() => { this.handleRemove(cartProduct.id) }}></i>
                         </div>
                     </div>
 
                 </li>
             );
         });
-        
+
 
         return (
             <div className="wrap cf">
@@ -67,4 +75,18 @@ class CartComponent extends Component {
 
 }
 
-export default connect(mapStateToProps)(CartComponent);
+const mapStateToProps = (state) => {
+    return {
+        products: state.addedProducts,
+        total: state.total
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removeItem: (id) => { dispatch(removeItem(id)) },
+        addQuantity: (id) => { dispatch(addQuantity(id)) },
+        subtractQuantity: (id) => { dispatch(subtractQuantity(id)) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CartComponent);
--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -3,22 +3,77 @@ import Product2 from '../images/product2.png'
 import Product3 from '../images/product3.png'
 import Product4 from '../images/product4.png'
 import Product5 from '../images/product5.png'
-
+import { REMOVE_ITEM, SUB_QUANTITY, ADD_QUANTITY } from './ActionTypes'
 
 const initState = {
     addedProducts: [
-        {id:1,title:'Jacket',code:"#594786", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:200,img:Product1,quantity:1},
-        {id:2,title:'T Shirt',code:"#364251", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:100,img: Product2,quantity:1},
-        {id:3,title:'Suit',code:"#956784", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:260,img: Product3,quantity:1},
-        {id:4,title:'Short',code:"#125496", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:200,img:Product4,quantity:1},
-        {id:5,title:'Nike shoes',code:"#836542", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Product5,quantity:1},
+        { id: 1, title: 'Jacket', code: "#594786", price: 200, img: Product1, quantity: 1 },
+        { id: 2, title: 'T Shirt', code: "#364251", price: 100, img: Product2, quantity: 1 },
+        { id: 3, title: 'Suit', code: "#956784", price: 260, img: Product3, quantity: 1 },
+        { id: 4, title: 'Short', code: "#125496", price: 200, img: Product4, quantity: 1 },
+        { id: 5, title: 'Nike shoes', code: "#836542", price: 160, img: Product5, quantity: 1 },
     ],
     total: 920
 
 }
-const cartReducer= (state = initState,action)=>{
-    
-    return state;
+const cartReducer = (state = initState, action) => {
+
+    switch (action.type) {
+
+        //remove products from cart
+        case REMOVE_ITEM: {
+
+            let itemToRemove = state.addedProducts.find(item => action.id === item.id)
+            let new_items = state.addedProducts.filter(item => action.id !== item.id)
+
+            //calculating the total after delete the product
+            let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity)
+            return {
+                ...state,
+                addedProducts: new_items,
+                total: newTotal
+            }
+        }
+
+        //Increase quanity of the product and calc the new total
+        case ADD_QUANTITY: {
+
+            let addedItem = state.addedProducts.find(item => item.id === action.id)
+            addedItem.quantity += 1
+            let newTotal = state.total + addedItem.price
+            return {
+                ...state,
+                total: newTotal
+            }
+        }
+
+        //Decrease quanity of the product and calc the new total
+        case SUB_QUANTITY: {
+
+            let addedItem = state.addedProducts.find(item => item.id === action.id)
+            //if the qt == 0 then it should be removed
+            if (addedItem.quantity === 1) {
+                let new_items = state.addedProducts.filter(item => item.id !== action.id)
+                let newTotal = state.total - addedItem.price
+                return {
+                    ...state,
+                    addedProducts: new_items,
+                    total: newTotal
+                }
+            }
+            else {
+                addedItem.quantity -= 1
+                let newTotal = state.total - addedItem.price
+                return {
+                    ...state,
+                    total: newTotal
+                }
+            }
+        }
+        default:
+            return state;
+    }
+
 
 }
 export default cartReducer;

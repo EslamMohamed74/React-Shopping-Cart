cf64a482d30a68920e50f6d967c3a7cc2abe32c3
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-09-22 00:21:44 +0200

    Updates

---
+++ precache-manifest.4e0eb02aff9b7b3127cba4ac8147be4c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c6ebdc9528c9daf251caa154bd62b1",
+    "url": "/React-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "0dc37028d0185c132113",
+    "url": "/React-Shopping-Cart/static/css/main.e5c587ce.chunk.css"
+  },
+  {
+    "revision": "edecec33fe9e579f1b42",
+    "url": "/React-Shopping-Cart/static/js/2.1889ef5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Shopping-Cart/static/js/2.1889ef5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc37028d0185c132113",
+    "url": "/React-Shopping-Cart/static/js/main.a64989b3.chunk.js"
+  },
+  {
+    "revision": "3f25119fa86995fd2c0e",
+    "url": "/React-Shopping-Cart/static/js/runtime-main.0911f2e6.js"
+  },
+  {
+    "revision": "bc727437f38bb728f82abf2553fe0dc7",
+    "url": "/React-Shopping-Cart/static/media/product1.bc727437.png"
+  },
+  {
+    "revision": "ce773f1f5908a989b486ffc194c703ab",
+    "url": "/React-Shopping-Cart/static/media/product2.ce773f1f.png"
+  },
+  {
+    "revision": "28425b2269fcc4e38cdbd2be549d98bd",
+    "url": "/React-Shopping-Cart/static/media/product3.28425b22.png"
+  },
+  {
+    "revision": "07cba3e47dcd2cbc3355c8c2f02bb87e",
+    "url": "/React-Shopping-Cart/static/media/product4.07cba3e4.png"
+  },
+  {
+    "revision": "dbac4f6ea2ed2416c8c4f266aaf40921",
+    "url": "/React-Shopping-Cart/static/media/product5.dbac4f6e.png"
+  }
+]);
